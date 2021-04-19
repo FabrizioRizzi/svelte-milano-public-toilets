b71ef96d2ade59da9d560b1099c7d11305974d91
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2021-04-20 00:23:31 +0200

    Added municipio filter

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export interface PublicToilet {
+  properties: {
+    DISPONIBILITA: string;
+    ID_NIL: string;
+    LAT_Y_4326: number;
+    LOCALITA: string;
+    LONG_X_4326: number;
+    Location: string;
+    MUNICIPIO: string;
+    NIL: string;
+    OBJECTID: string;
+    TIPO: string;
+    ULTERIORI_INFO: string | null;
+    VIA: string;
+  };
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,55 @@
 <script lang="ts">
-  import Header from '$lib/Header.svelte';
+	import Header from '$lib/Header.svelte';
+	import type { PublicToilet } from 'src/interfaces';
 
-  const loadJSON = async () => {
-    try {
-      const res = await fetch('static/data/servizi_igienici_pubblici.geojson');
-      return await res.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	let toilets: PublicToilet[];
+	let filteredToilets: PublicToilet[];
+	let municipio: number;
 
-  let promise = loadJSON();
+	const loadJSON = async () => {
+		try {
+			const res = await fetch('static/data/servizi_igienici_pubblici.geojson');
+			const json = await res.json();
+			toilets = json.features;
+			filteredToilets = json.features;
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  function handleClick() {
-    promise = loadJSON();
-  }
+	loadJSON();
+
+	let promise = loadJSON();
+
+	function handleClick() {
+		if (municipio) {
+			filteredToilets = toilets.filter((a) => a.properties.MUNICIPIO === municipio.toString());
+		} else {
+			filteredToilets = toilets;
+		}
+	}
 </script>
 
 <svelte:head>
-  <title>Milano Public Toilets</title>
+	<title>Milano Public Toilets</title>
 </svelte:head>
 
 <main>
-  <Header />
-
-  <button on:click={handleClick}>Refresh</button>
-  {#await promise}
-    <p>...loading</p>
-  {:then todos}
-    <span>{todos?.features[0].properties.MUNICIPIO}</span>
-  {:catch error}
-    <p style="color: red">{error}</p>
-  {/await}
+	<Header />
+	<input type="number" min=1 max=9 bind:value={municipio} />
+	<button on:click={handleClick}>Refresh</button>
+	{#if toilets}
+		<ul>
+			{#each filteredToilets as toilet}
+				<li>{toilet.properties.VIA + toilet.properties.LOCALITA}</li>
+			{/each}
+		</ul>
+	{/if}
 </main>
 
 <style lang="scss">
-  main {
-    text-align: center;
-    margin: 0 auto;
-  }
+	main {
+		text-align: center;
+		margin: 0 auto;
+	}
 </style>
